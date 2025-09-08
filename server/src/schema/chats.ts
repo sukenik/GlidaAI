fb844971d1b6208cfa254633e4d3151d90afb7f5
Daniel Sukenik <sukenik9@gmail.com>
2025-09-08 22:08:13 +0300

    Removed redundant

--- server/src/schema/chats.ts
+++ server/src/schema/chats.ts
@@ -93,7 +93,7 @@ async function getChat(chatId: string): Promise<iChat | null> {
 
 async function addMessage(chatId: string, content: string): Promise<void> {
     try {
-        const chatRef = await db.collection(CHATS_COLLECTION).doc(chatId)
+        const chatRef = db.collection(CHATS_COLLECTION).doc(chatId)
 		const messagesRef = chatRef.collection(MESSAGES_COLLECTION)
         
         const response = await generateResponse(content)
